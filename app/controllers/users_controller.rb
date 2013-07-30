9731b8103c7aecb6e7ba759a9d83c75dc6e85103
Ahmed O <aomra015@gmail.com>
2013-07-29 21:20:10 -0400

    remove broken avatar code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :email, :image_url, :handle, :avatar, :remove_avatar, :avatar_two, :remove_avatar_two)
+    params.require(:user).permit(:name, :email, :image_url, :handle, :avatar, :remove_avatar)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,4 @@ class User < ActiveRecord::Base
   has_many :tweets
 
   mount_uploader :avatar, AvatarUploader
-  mount_uploader :avatar_two, AvatarUploader
 end
